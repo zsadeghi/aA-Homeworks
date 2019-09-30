bc89b0360316678b3c6791bfceccf3cab9738890
Zohreh Sadeghi <zsadeghi@uw.edu>
2019-09-30 01:08:14 -0700

    done with phase 2

--- W7D1/auth_prac/app/models/user.rb
+++ W7D1/auth_prac/app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+    validates :username, presence: true
+    validates :session_token, presence: true
+    validates :password_digest, presence: { message: "Password can't be blank"} 
+    validates :password, length: { minimum: 8}, allow_nil: true
+    before_validation :ensure_session_token, on: [:create, :update]
+
+    # def ensure_session_token
+
+    # end
+
 end
